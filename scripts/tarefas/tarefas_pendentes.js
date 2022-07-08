66a49fcce5b920897ed94149f1920f32eca12d9e
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2022-07-08 14:40:20 -0300

    incluido v6 do font awesome para os icones

--- scripts/tarefas/tarefas_pendentes.js
+++ scripts/tarefas/tarefas_pendentes.js
@@ -9,8 +9,8 @@ function renderizaTarefasPendentes(tarefa) {
     <button class="not-done" id="${tarefa.id}" onClick="atualizarStatusTarefa(this.id, false)" ></button> 
     <div class="descricao">
         <p class="nome">${tarefa.description}</p>
-        
-        <p class="timestamp"> <button id="${tarefa.id}" onClick="atualizarTextoTarefa(this.id)"><i class="fas fa-pen"></i> </button>  <i class="far fa-calendar-alt"></i> ${tarefa.createdAt}
+        <p class="timestamp"> <button id="${tarefa.id}" onClick="atualizarTextoTarefa(this.id)"><i class="fas fa-pen"></i></button>  
+        <i class="far fa-calendar-alt"></i> ${tarefa.createdAt}
         <button id="${tarefa.id}" onClick="deletarTarefa(this.id)"><i class="far fa-trash-alt"></i></button></p>
         </div>
     `
